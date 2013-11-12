34dea67bfead9b71001be64457c95398e418ae2c
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2013-11-13 01:26:45 +0800

    Update node_modules sub module. Added search-file procedure (wip)

---
+++ javascript/search-file.js
@@ -0,0 +1,8 @@
+var childprocess = require( "child_process" );
+var async = require( "async" );
+var _ = require( "underscore" );
+var fs = require( "fs" );
+
+var searchFile = function searchFile( filename, path, callback ){
+	
+};
